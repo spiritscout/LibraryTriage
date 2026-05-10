6d9e674eacbc971a8766b86c77136f25a02a2e32
spiritscout <spdx5651@leeds.ac.uk>
2026-05-10 05:46:36 +0100

    FFprobeRunner declaration

---
+++ src/LibraryTriage.Core/FFprobe/FFprobeRunner.cs
@@ -0,0 +1,12 @@
+//contains Process for spawning and controlling FFprobe
+using System.Diagnostics;
+
+//parent that contains deserializer
+using System.Text.Json;
+
+namespace LibraryTriage.Core.FFprobe;
+
+public class FFprobeRunner
+{
+    
+}

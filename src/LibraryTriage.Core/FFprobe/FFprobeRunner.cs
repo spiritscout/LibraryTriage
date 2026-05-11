788916b3b4254430e3a85ac25263f50f2b21ef73
spiritscout <spdx5651@leeds.ac.uk>
2026-05-11 23:35:39 +0100

    Add FFproberunner class with FFprobe output method

--- src/LibraryTriage.Core/FFprobe/FFprobeRunner.cs
+++ src/LibraryTriage.Core/FFprobe/FFprobeRunner.cs
@@ -8,5 +8,26 @@ namespace LibraryTriage.Core.FFprobe;
 
 public class FFprobeRunner
 {
-    
+    public async Task<FFprobeOutput> RunAsync(string filePath)
+    {
+        var process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "ffprobe",
+                Arguments = $"-v quiet -print_format json -show_streams -show_format \"{filePath}\"",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            }
+        };
+
+        process.Start();
+        string json = await process.StandardOutput.ReadToEndAsync();
+        await process.WaitForExitAsync();
+        return JsonSerializer.Deserialize<FFprobeOutput>(json, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+    }
 }
